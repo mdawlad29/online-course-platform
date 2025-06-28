4a58cfb3da65be7bd83a6780a33ede3a0531441a
mdawlad29 <mdawladbdinfo@gmail.com>
2025-06-28 11:53:25 +0600

    Page scroll to active header

--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NotificationIconComponent } from '../../icons/notification-icon';
 import { CartIcon } from '../../icons/cart-icon';
 import { FavoriteIcon } from '../../icons/favorite-icon';
 import { LogoComponent } from '../../shared/logo/logo.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
+    NgClass,
     RouterLink,
     CartIcon,
     FavoriteIcon,
@@ -17,4 +19,11 @@ import { LogoComponent } from '../../shared/logo/logo.component';
   ],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  scrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrolled = window.scrollY > 100;
+  }
+}
