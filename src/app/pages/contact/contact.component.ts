9f327e1cc2bb39513452bef4c141d3816efee4b5
mdawlad29 <mdawladbdinfo@gmail.com>
2025-07-01 16:49:04 +0600

    Build issue resolved

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -4,8 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-contact',
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
